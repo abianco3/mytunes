24d75aaf099b0ed23d4df9ddd55d8163761d3c23
abianco3 <anthony.bianco3@gmail.com>
2017-02-20 21:23:07 -0800

    Pass Songs parse tests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -4,10 +4,17 @@ var Songs = Backbone.Collection.extend({
   model: SongModel,
   url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
   initialize: function() {
-    
+    this.fetch({
+      success: function(data) {
+        this.add(data.models);
+      }.bind(this),
+      error: function(data) {
+        console.log('failed');
+      }
+    });
   },
   parse: function(data) {
-    return data.songs;
+    return data.results;
   }
 
 });
