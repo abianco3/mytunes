0d4226ec570e4f5db71a8c754afa1490afcd1825
abianco3 <anthony.bianco3@gmail.com>
2017-02-20 20:40:21 -0800

    Pass tests for PlayerView

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,13 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+  url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
+  initialize: function() {
+    
+  },
+  parse: function(data) {
+    return data.songs;
+  }
 
 });
