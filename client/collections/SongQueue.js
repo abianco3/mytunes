60bdad5749ae5f34628d87f96640dae9cf9d3497
abianco3 <anthony.bianco3@gmail.com>
2017-02-20 19:00:43 -0800

    Pass tests for SongModel and SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,26 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function(song) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
 
+    this.on('ended', function(song) {
+      this.shift();
+      if (this.length) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+  },
+  
+  playFirst: function() {
+    //return _.first(this.collection);
+    this.at(0).play();
+  }
 });
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,6 +7,7 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
+      // Should add to Song Queue
       this.model.play();
     }
   },
