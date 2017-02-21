1d64cd1dba2db6d2416c3432974271418971e3dd
abianco3 <anthony.bianco3@gmail.com>
2017-02-21 11:36:36 -0800

    Update PlayerView to show artwork for currently playing song

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -3,7 +3,8 @@ var PlayerView = Backbone.View.extend({
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
-  el: '<audio controls autoplay />',
+  //el: '<audio controls autoplay />',
+  template: _.template('<img src=<%= artwork_url %>> <audio controls autoplay src= <%= url %> />'),
 
   initialize: function() {
     this.$el.on('ended', function() {
@@ -18,7 +19,8 @@ var PlayerView = Backbone.View.extend({
   },
 
   render: function() {
-    return this.$el.attr('src', this.model ? this.model.get('url') : '');
+    // return this.$el.attr('src', this.model ? this.model.get('url') : '');
+    return this.$el.html(this.template(this.model.attributes));
   }
 
 });
